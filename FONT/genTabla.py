81a2f020a932cd40a21c04dd8584e6bfeaa22705
elorenteg <ester.lorente@outlook.com>
2015-06-07 02:53:22 +0200

    mejora de tablas

--- FONT/genTabla.py
+++ FONT/genTabla.py
@@ -9,25 +9,46 @@ ifile = open('datos.csv', "r")
 reader = csv.reader(ifile, delimiter='\t')
 RATIO = []
 TIEMP = []
+TTOTA = []
+incT = 0
+incP = 0
+calculado = False
 for row in reader:
     ntareas = row[0]
     nprogs = row[1]
     tiempo = row[2]
+    ttotal = row[3]
     rat = int(ntareas)/int(nprogs)
     TIEMP.append(tiempo)
     RATIO.append(rat)
+    TTOTA.append(ttotal)
     print ntareas + " -- " + nprogs + " -- " + tiempo + " -- " + str(rat)
+    
+    if incT == 0:
+        incT = int(ntareas)
+        incP = int(nprogs)
+    elif not calculado:
+        incT = int(ntareas) - incT
+        incP = int(nprogs) - incP
+        calculado = True
 
-pl.figure(figsize=(8, 6), dpi=80) # Create a figure of size 8x6 inches, 80 dots per inch
-pl.subplot(1, 1, 1) # Create a new subplot from a grid of 1x1
+pl.figure(figsize=(15, 6), dpi=80) # Create a figure of size 8x6 inches, 80 dots per inch
+pl.subplot(1, 2, 1) # Create a new subplot from a grid of 1x1
 
-ratio = [4,5]
-tiempo = [1,2]
-pl.plot(RATIO,TIEMP, marker='o')
-
-pl.xlabel('Ratio ntareas/nprogs')
+pl.plot(range(1,len(TIEMP)+1),TIEMP, marker='o', label='inc nt('+str(incT)+') np('+str(incP)+')')
+pl.grid(True)
+pl.xlabel('Num. de ejecucion')
 pl.ylabel('Tiempo de ejecucion (s)')
-pl.title('Tiempo segun el ratio ntareas/nprogs')
+pl.title('Tiempo segun incrementamos el tamano')
+pl.legend()
+pl.subplot(1, 2, 2)
+pl.plot(range(1,len(TTOTA)+1),TTOTA, marker='o', label='inc nt('+str(incT)+') np('+str(incP)+')')
+pl.grid(True)
+pl.xlabel('Num. de ejecucion')
+pl.ylabel('Tiempo de realizacion (h)')
+pl.title('Tiempo segun incrementamos el tamano')
+pl.legend()
+
 
 pl.savefig("graph.png", dpi=72) # Save figure using 72 dots per inch
 pl.show() # Show result on screen
